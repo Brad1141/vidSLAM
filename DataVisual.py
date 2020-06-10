b6717ed761a8495f24dce1f17682c258e1eda78b
brad1141 <bradlamontagne1141@gmail.com>
2020-06-10 00:24:53 -0400

    fixing 3d map

--- DataVisual.py
+++ DataVisual.py
@@ -4,47 +4,13 @@ from matplotlib import pyplot as plt
 
 import cv2
 import numpy as np
+import math
 
 prevKP = []
 prevDes = []
 prevImg = []
 
-def printPic(bytes):
-    # PNG data
-    LEFT_THUMB = (
-        b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A\x00\x00\x00\x0D\x49\x48\x44\x52\x00\x00'
-        b'\x00\x13\x00\x00\x00\x0B\x08\x06\x00\x00\x00\x9D\xD5\xB6\x3A\x00\x00\x01'
-        b'\x2E\x49\x44\x41\x54\x78\x9C\x95\xD2\x31\x6B\xC2\x40\x00\x05\xE0\x77\x10'
-        b'\x42\x09\x34\xD0\x29\x21\x82\xC9\x9C\x2E\x72\x4B\x87\x40\x50\xB9\xBF\x5B'
-        b'\x28\x35\xA1\xA4\x94\x76\x68\x1C\x1C\x74\xCD\x9A\xE8\x20\x0A\x12\xA5\x5A'
-        b'\xE4\x72\xC9\x75\x10\x6D\xDC\xCE\xF7\x03\x3E\xDE\x83\x47\xA4\x94\x68\x67'
-        b'\xB5\xD9\x4E\xBF\xBF\x3E\xE8\x78\x3C\x86\x6A\x3C\xCF\x43\x10\x04\x20\x6D'
-        b'\x6C\xB5\xD9\x4E\x93\xF8\x95\x5A\x96\x05\xC6\x98\x32\x56\x14\x05\x46\xA3'
-        b'\x11\xB4\x36\x14\xBD\x3C\xD3\x4E\xA7\x03\xC6\x18\x8E\xC7\x23\x9A\xA6\x51'
-        b'\xC2\x5C\xD7\x45\x9E\xE7\x27\xEC\x0C\x39\x8E\x03\xC6\x18\x0E\x87\x83\x32'
-        b'\x04\x00\xE7\x75\x1A\xE7\x7C\xF2\xF9\xFE\x46\x6D\xDB\x06\x63\x0C\xFB\xFD'
-        b'\x1E\x75\x5D\x2B\x43\x57\x58\xF9\xF3\xAB\xAD\xD7\x6B\x98\xA6\x09\x21\x04'
-        b'\x76\xBB\x1D\x84\x10\x37\x61\x86\x61\x9C\x30\x00\x70\x1C\x07\x49\x92\x80'
-        b'\x10\x82\x7E\xBF\x8F\xE5\x72\x79\x13\x78\x69\xF6\x70\x6F\x88\x5E\xAF\x37'
-        b'\x2B\xCB\x92\xC6\x71\x0C\x42\x08\xC2\x30\xC4\x7C\x3E\x57\x06\x2F\x98\xAE'
-        b'\xEB\x4F\xAE\xEB\x4E\x06\x83\xC1\x4C\x4A\x49\xA3\x28\x82\x94\x12\x61\x18'
-        b'\x2A\x37\x5B\x2C\x16\xE8\x76\xBB\xFF\x3F\xE3\x9C\x4F\x8A\xA2\xD0\xD2\x34'
-        b'\xA5\x59\x96\xA1\xAA\x2A\x65\xCC\xB2\x2C\x0C\x87\xC3\xEB\xD3\x9E\xC1\xAA'
-        b'\xAA\xEE\x38\xE7\x4A\x90\xAE\xEB\x00\x00\xDF\xF7\x1F\xFF\x00\x09\x7C\xA7'
-        b'\x93\xB1\xFB\xFA\x11\x00\x00\x00\x00\x49\x45\x4E\x44\xAE\x42\x60\x82'
-    )
-
-    stream = BytesIO(LEFT_THUMB)
-
-    image = Image.open(stream).convert("RGBA")
-    stream.close()
-    image.show()
-
 def featureExtraction(frame):
-
-    corners = []
-    countI = 0
-    countJ = 0
 
     # Initiate STAR detector
     orb = cv2.ORB_create()
@@ -55,8 +21,8 @@ def featureExtraction(frame):
     # draw only keypoints location,not size and orientation
     img2 = cv2.drawKeypoints(frame, kp, None, color=(0, 0, 255), flags=0)
 
-    cv2.imshow('corners', img2)
-    #dataAssociation(kp, des, frame)
+    cv2.imshow('drive', img2)
+    dataAssociation(frame)
 
 def dataAssociation(currImg):
     global prevKP, prevImg, prevDes
@@ -114,13 +80,59 @@ def orbCompare():
     cv2.imshow('corners', img3)
     cv2.waitKey()
 
-def essentialMatrix():
-    test = cv2.findEssentialMat()
+def buildMap(matches, kp1, kp2):
+
+    points1 = []
+    points2 = []
+
+    #horizontal(x) and vertical(y) length of image from center
+    x = 600 / 2
+    y = 600 / 2
+
+
+    for mat in matches:
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        # Get the coordinates
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
+
+        # Append to each list
+        points1.append((x1, y1))
+        points2.append((x2, y2))
+
+    #focal lengths (assumes that the field of view is 60)
+    f_x = x / math.tan(60 / 2)
+    f_y = y / math.tan(60 / 2)
+
+    #camera matrix
+    K = np.array([[f_x, 0, x],
+                  [0, f_y, y],
+                  [0, 0, 1]])
+
+    E = cv2.findEssentialMat(points1, points2, K)
+
+    U, S, vh = np.linalg.svd(E)
+
+    w_i = np.array([[0, 1, 0],
+                    [-1, 0, 0],
+                    [0, 0, 1]])
+    z = np.array([[0, 1, 0],
+                  [-1, 0, 0],
+                 [0, 0, 0]])
+    t = np.asmatrix(U) * np.asmatrix(z) * np.asmatrix(U).T
+
+    R = np.asmatrix(U) * np.asmatrix(w_i) * np.asmatrix(vh).T
+
+    for i in range(len(kp1)):
+        x3 = ((R[0] - kp2[i]))
+
+
     return
-
-
-
-
 
 class coordinates:
     #x_ij: the 3D position of the point with respect to the camera
